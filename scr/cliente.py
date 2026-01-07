2ba8b2fab84874aaad399cb8d5add6c256cf5ce8
Vitória Cavalcante Souza <vitoria.csouza@outlook.com>
2026-01-07 11:22:13 -0300

    Update cliente.py

--- scr/cliente.py
+++ scr/cliente.py
@@ -1,5 +1,9 @@
 class Cliente:
-    def __init__(self, id_cliente: int, nome: str, email: str, cpf: str, enderecos: List[str]):
+    def __init__(self, id_cliente: int, nome: str, email: str, cpf: str, enderecos: List):
+        if "@" not in email:
+            raise ValueError("Email inválido")
+        if len(cpf) < 11:
+            raise ValueError("CPF inválido")
         self.id = id_cliente
         self.nome = nome
         self.email = email
@@ -12,8 +16,8 @@ class Cliente:
             raise ValueError("Email inválido")
         self.email = novo_email
 
-    def excluir(self):
-        self.ativo = False
+    def __eq__(self.other):
+        return self.cpf == other.cpf or self.email == other.email
 
     def __str__(self):
         return f"Cliente {self.id} | {self.nome} | {self.email}"
