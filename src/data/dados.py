a61b985be95ad641a5e12d9b128f8f00a95723b7
Carlos Rodrigo Ferreira <rodrigocfs13@gmail.com>
2026-01-18 21:26:37 -0300

    Update dados.py

--- src/data/dados.py
+++ src/data/dados.py
@@ -1,1 +1,16 @@
+import json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "database.json"
+
+
+def carregar_dados():
+    with open(DB_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def salvar_dados(dados):
+    with open(DB_PATH, "w", encoding="utf-8") as f:
+        json.dump(dados, f, indent=4, ensure_ascii=False)
 
